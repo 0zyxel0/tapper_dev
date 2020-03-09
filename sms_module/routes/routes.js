ade12ebb9433330d961ca0a82cde06fc86a1d482
0zyxel0 <calvin.ongkingco@gmail.com>
2020-03-09 17:17:40 +0100

    1

--- sms_module/routes/routes.js
+++ sms_module/routes/routes.js
@@ -7,7 +7,7 @@ const httpClient = require('urllib');
 const bodyParser = require('body-parser');
 
 //Declaring constants
-const device = 'COM1';
+const device = 'COM3';
 const modem = require('modem-commands').Modem();
 const modemOptions = {
   baudRate: 115200,
